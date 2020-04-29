1d19c022cdf76550364e9fbcad253aae1be653ae
Cam Manderson <cam@alpaca.travel>
2020-04-29 11:13:49 +1000

    Adds in preset env targeting

--- packages/graph-sdk/babel.config.js
+++ packages/graph-sdk/babel.config.js
@@ -4,10 +4,14 @@ module.exports = {
     [
       '@babel/preset-env',
       {
+        useBuiltIns: 'entry',
+        corejs: '2',
         targets: {
-          node: 'current',
+          node: '8',
+          browsers: ['>0.2%', 'not dead', 'not ie < 11', 'not op_mini all'],
         },
       },
     ],
   ],
+  plugins: ['@babel/plugin-transform-runtime'],
 };
